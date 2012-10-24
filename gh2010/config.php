91b6bb66bf9c0e66a91559d99cf83b6c208846d2
CornGuo <cornguo@msn.com>
2012-10-24 17:05:52 +0800

    - add config.php for setting basic info

---
+++ gh2010/config.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Moovee -- 2010 Golden Horse Film Festival";
+$description = "很陽春的排片單系統";
+$keywords = "排片單, 2010, 金馬, 金馬影展, Golden Horse 2010";
+$extlinks = "<a class=\"navbar-link\" href=\"http://playpcesor.blogspot.com/2009/10/moovee-2009.html\" title=\"使用說明 by 電腦玩物\">使用說明</a> |
+             <a class=\"navbar-link\" href=\"http://www.goldenhorse.org.tw/\">2010 台北金馬影展官方網站</a> |
+             <a class=\"navbar-link\" href=\"2010_Golden_Horse_Programme_Book.pdf\">影展手冊 PDF</a>";
+$remarkDesc ="★ 影人出席或導讀；詳情請見網站及現場<br />
+▲ 影片拷貝非英語發音且無英文字幕 (註：有中文發音或中文字幕)<br />
+◎ 非售票之活動場次；入場方式請見活動頁<br />
+(跨日場次將以大於 24 小時方式表示)";
---
+++ gh2011f/config.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Moovee -- 2011 Golden Horse Fantastic Film Festival";
+$description = "很陽春的排片單系統";
+$keywords = "排片單, 2011, 金馬, 金馬影展, 奇幻影展, Golden Horse 2011";
+$extlinks = "<a class=\"navbar-link\" href=\"http://playpcesor.blogspot.com/2009/10/moovee-2009.html\" title=\"使用說明 by 電腦玩物\">使用說明</a> |
+             <a class=\"navbar-link\" href=\"http://www.ghfff.org.tw/home.aspx\">2011 台北金馬奇幻影展官方網站</a> |
+             <a class=\"navbar-link\" href=\"2011_GHFFF_Programme.pdf\">影展手冊 PDF</a>";
+$remarkDesc = "★ 影人出席或導讀；詳情請見網站及現場<br />
+▲ 影片拷貝非英語發音且無英文字幕 (註：有中文發音或中文字幕)<br />
+◎ 非售票之活動場次；入場方式請見活動頁<br />
+(跨日場次將以大於 24 小時方式表示)";
