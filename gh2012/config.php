df5c913287310af6a6c7f6a4e9277556d0ba8de3
John Lee <john@0xlab.org>
2012-10-30 00:05:19 +0800

    add gh2012 by copying tiff2011

---
+++ gh2012/config.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Moovee -- 2011 Taipei Film Festival";
+$description = "很陽春的排片單系統";
+$keywords = "排片單, 2011, 台北電影節, 影展, Taipei Film Festival, TIFF";
+$extlinks = "<a class=\"navbar-link\" href=\"http://playpcesor.blogspot.com/2009/10/moovee-2009.html\" title=\"使用說明 by 電腦玩物\">使用說明</a> |
+             <a class=\"navbar-link\" href=\"http://www.taipeiff.org.tw/Default.aspx\">2011 台北電影節官方網站</a> |
+             <a class=\"navbar-link\" href=\"tiff2011_booklet.pdf\">影展手冊 PDF</a> |
+             <a class=\"navbar-link\" href=\"http://www.taipeiff.org.tw/Public/Content.aspx?id=61&amp;subid=5192\">已售完場次</a>";
+$remarkDesc = "★ 導演或影人出席映後或映前座談<br />
+▲ 影片拷貝非英語發音且無英文字幕<br />
+◎ 免費入場<br />
+(跨日場次將以大於 24 小時方式表示)";
