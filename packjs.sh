6bd8186be67542b568978dcfa4218c231945bc1e
John Lee <john@0xlab.org>
2012-10-30 01:07:59 +0800

    'sh' is suffice

--- packjs.sh
+++ packjs.sh
@@ -1,4 +1,4 @@
-#!/bin/tcsh
+#!/bin/sh
 ./packjs/jsmin/jsmin < devel/func.js > func-min.js
 php packjs/packer/pack.php func-min.js func.js
 rm -f func-min.js
--- packjs.test.sh
+++ packjs.test.sh
@@ -1,4 +1,4 @@
-#!/bin/tcsh
+#!/bin/sh
 ./packjs/jsmin/jsmin < devel/func.test.js > func-min.js
 php packjs/packer/pack.php func-min.js func.test.js
 rm -f func-min.js
